d3ba4af6743f35d9046a86de9b1d37bbb14f45fb
nastya <nastya.vorono@gmail.com>
2024-07-16 15:14:41 +0300

    вывод данных

--- kurs.js
+++ kurs.js
@@ -1,2 +1,19 @@
-let a;
-a=12;
+const numberOfFilms=+prompt('Склько фильмов Вы уже посмотрели?','');
+
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+
+      console.log(personalMovieDB);
+      
