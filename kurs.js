7f5a934119b886927e89c464b0771b8c9e99e4e3
nastya <nastya.vorono@gmail.com>
2024-07-16 15:32:57 +0300

    условия

--- kurs.js
+++ kurs.js
@@ -1,19 +1,34 @@
-const numberOfFilms=+prompt('Склько фильмов Вы уже посмотрели?','');
+"use strict"
 
-const personalMovieDB={
-    count:numberOfFilms,
-    movies:{},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+if (4){
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
 
-      personalMovieDB.movies[a]=b;
-      personalMovieDB.movies[c]=d;
+//const n=50;
 
-      console.log(personalMovieDB);
-      
+if (n<49){
+    console.log('error');
+} else{
+    if (n>100){
+        console.log('много');
+    } else{ 
+        console.log('ok')
+            
+        }
+    }
+    //тернарный оператор 
+   
+      (n==50)?console.log('ok'):console.log('error');
+
+      const n=50;
+        switch (n){
+            case 49:console.log('no');
+            break;
+            case 51: console.log('yes');
+            break;
+            default: console.log('nnnn');
+            break;
+
+        }
